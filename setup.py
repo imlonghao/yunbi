2055b6ef446723c214f2a2a70f935f0cabf68d25
imlonghao <shield@fastmail.com>
2017-08-01 22:13:08 +0800

    Release v0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='yunbi',
-    version='0.1.1',
+    version='0.1.2',
     keywords=('Yunbi'),
     url='https://github.com/imlonghao/yunbi',
     license='MIT License',
